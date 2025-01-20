65d051d4d4778f9a0f4703deb24314e3afce76b0
Ritesh Vesalapu <43720654+ritesh-1227@users.noreply.github.com>
2025-01-20 00:49:03 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,3 +18,10 @@ if st.button('Get Recommendations'):
             st.text("No recommendations available.")
     else:
         st.write("Please enter a User ID.")
+
+# Adding the note at the bottom
+st.markdown("---")  # Adds a horizontal rule for separation
+st.markdown(
+    "_**Note:** This system is for demonstration purposes only. "
+    "Providing a list of all usernames is not practical in this case. Please try out the test names. If you need more, you can always reach out"
+)
